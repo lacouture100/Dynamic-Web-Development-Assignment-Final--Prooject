d6ced6fccfc798a9c5049fb44f8d5b3973b04d83
Alvaro Lacouture <lacouture100@gmail.com>
2020-03-19 18:43:04 -0400

    Got scoket.io working

--- config.js
+++
@@ -1,9 +0,0 @@
-//Here we define the data we want to keep private
-require('dotenv').config();
-
-module.exports = {
-    //This makes the default port be 3000
-    PORT: process.env.PORT ||3000,
-    //This makes the default URI be ''
-    MONGODB_URI : process.env.MONGODB_URI || ''
-}
