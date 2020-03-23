3b9f3d096c31f64e052ca8db626731bd170a244e
Alvaro Lacouture <lacouture100@gmail.com>
2020-03-22 20:19:16 -0400

    Modified PI file

--- raspberryPi/pi-client.js
+++ raspberryPi/pi-client.js
@@ -1,7 +1,11 @@
 var io = require('socket.io-client');
-var socket = io.connect('https://https://websockets-playground-2020.herokuapp.com/',
+var socket = io.connect('https://websockets-playground-2020.herokuapp.com/',
     {reconnect: true, transports : ['websocket'], path: '/socket.io'});
 
+    console.log("PI on")
 socket.on('connect', function (socket) {
     console.log('Connected!');
 });
+
+
+setInterval(() => io.emit('piMessage', 'Hello from PI', 1000);
